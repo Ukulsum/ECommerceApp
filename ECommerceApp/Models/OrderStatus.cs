3d03b81e6f8f88df6b2a814d3ae2f61d391a63bb
ukulsum <ukulsum1280@gmail.com>
2025-03-26 04:11:51 +0600

    Add project files.

---
+++ ECommerceApp/Models/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommerceApp.Models
+{
+    // Enum to represent the status of an order
+    // The JsonStringEnumConverter ensures that enums are serialized as their string names
+    // instead of integer values, enhancing readability.
+    public enum OrderStatus
+    {
+        Pending = 1,
+        Processing = 2,
+        Shipped = 3,
+        Delivered = 4,
+        Canceled = 5
+    }
+}
