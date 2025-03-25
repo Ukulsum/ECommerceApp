6bb83be650005e215f5edd3c9ea01392e09dd9d2
ukulsum <ukulsum1280@gmail.com>
2025-03-26 04:50:06 +0600

    df

--- ECommerceApp/Program.cs
+++ ECommerceApp/Program.cs
@@ -1,11 +1,21 @@
+using ECommerceApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    // This will use the property names as defined in the C# model
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure EF Core with SQl Server
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EFCoreDbConnection")));
 
 var app = builder.Build();
 
